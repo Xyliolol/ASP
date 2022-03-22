5c18892ef7c0d53b8e882e4db1de774208ffd3f8
Deo <jeeytis@yandex.ru>
2022-03-22 21:11:56 +0300

    Lesson5

---
+++ MetricsManager/MetricsManager/Controllers/RamMetricsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetricsManager.Controllers
+{
+    [Route("api/metrics/ram")]
+    [ApiController]
+    public class RamMetricsController : ControllerBase
+    {
+        private readonly ILogger<RamMetricsController> _logger;
+
+        public RamMetricsController(ILogger<RamMetricsController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog встроен в CpuMetricsController");
+        }
+
+        [HttpGet("available/agent{agentId}")]
+        public IActionResult Available([FromRoute] int agentId)
+        {
+            _logger.LogInformation($"Получение RAM у \t {agentId}",
+               agentId);
+            return Ok();
+        }
+
+        [HttpGet("available/cluster")]
+        public IActionResult GetMetricsFromAllCluster()
+        {
+            _logger.LogInformation("Получение RAM со всех портов");
+            return Ok();
+        }
+    }
+}
